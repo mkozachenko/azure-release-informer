f0ff95df5acbd182104f3ab6f4370bd8735fd3bd
Maksym Kozachenko <symphonicon@gmail.com>
2020-08-21 11:55:39 +0300

    Update createWikiPage.ps1

--- createWikiPage.ps1
+++ createWikiPage.ps1
@@ -1,5 +1,6 @@
-$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($accessToken))
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":xajv7oeegbpycfvijhbrqs4cx7auyvuagrg5pzewpho32ptoirma"))
 $wikiPagePath = '/Release Notes'
+$pageExist = 'False'
 $Headers = @{
     'Authorization' = "Basic $encodedCreds"
     'Accept'        = "application/json"
@@ -18,16 +19,19 @@ $pagePath = $wikiPagePath+"/"+$currentSprintName+" sprint"
 # Проверяем список статей на наличие статьи для текущего спринта. Если статьи для спринта нет, создаем её
 foreach ($pageURL in $pages){
     if ($pageURL -eq $pagePath){
-        $JSONBody = @{
-            'Uri'         = "https://dev.azure.com/{organization}/{project}/_apis/wiki/wikis/{wikiName}/pages?path=$pagePath&api-version=5.1"
-            'Headers'     = $Headers
-            'Method'      = 'Put'
-            'ContentType' = 'application/json; charset=utf-8'
-            'body'        = @{content = "[[_TOC_]]"} | ConvertTo-Json
-        }
-        Invoke-RestMethod @JSONBody
+        $pageExist = 'True'
         break
     }
+}
+if ($pageExist -ne 'True'){
+    $JSONBody = @{
+        'Uri'         = "https://dev.azure.com/{organization}/{project}/_apis/wiki/wikis/{wikiName}/pages?path=$pagePath&api-version=5.1"
+        'Headers'     = $Headers
+        'Method'      = 'Put'
+        'ContentType' = 'application/json; charset=utf-8'
+        'body'        = @{content = "[[_TOC_]]"} | ConvertTo-Json
+    }
+    Invoke-RestMethod @JSONBody
 }
 
 Write-Host ('##vso[task.setvariable variable=wikiPagePath]'+$pagePath)
